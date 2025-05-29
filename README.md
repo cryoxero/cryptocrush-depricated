2d4b36a75c8f9c3dabe84069435b9c390110189c
cryoxero <cryoxero@proton.me>
2025-05-29 11:52:18 +0330

    Setup fastapi

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Cryptocrush
 
 A humble repository for everything related to cryptography, I'll update it as I go
+
+It's a server structure OF COURSE
+
+Built with django as the base and fastapi for api stuff
+
+## Run
+
+$ poetry install
+$ poetry shell
+$ poetry run python src/runserver.py
+
--- cryptocrush/src/cryptocrush/core/asgi.py
+++ cryptocrush/src/cryptocrush/core/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
+from fastapi import FastAPI
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cryptocrush.core.settings')
+django.setup()
+
+from cryptocrush.api.main import app as fastapi_app
 
 application = get_asgi_application()
+
+main_app = FastAPI()
+
+main_app.mount("/api", fastapi_app)
+main_app.mount("/", application)
+
